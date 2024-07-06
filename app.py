cff7c7c26c1149fc60261937da9d2d2b9efcd8c5
C_Walls <calebmarcuswalls@gmail.com>
2024-07-05 20:05:54 -0500

    fix dict indexing again...

--- app.py
+++ app.py
@@ -6,8 +6,9 @@ import tensorflow as tf
 def mnist_classifier(img):
     #img = tf.image.resize(img, [28, 28])
     #img = tf.cast(img, tf.float32)
-    print(img.keys())
-    return "returned: " + img.keys()
+    print(img['background'])
+    print(img['composite'])
+    return "returned: " + img['layers']
 
 demo = gr.Interface(fn=mnist_classifier, inputs="sketchpad", outputs="text", title="MNIST Checker", description="Draw a number 0-9 to see if the model can classify it.")
 demo.launch()
