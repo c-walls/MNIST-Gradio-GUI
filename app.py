a954d1505701c45324aab68284e2f735f1d78109
C_Walls <calebmarcuswalls@gmail.com>
2024-07-05 19:10:04 -0500

    Try new input

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import gradio as gr
 
 def greet(name):
-    return "Hello " + name + "!!"
+    return "Hi " + name + "!!"
 
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+demo = gr.Interface(fn=greet, inputs="sketchpad", outputs="label")
 demo.launch()
